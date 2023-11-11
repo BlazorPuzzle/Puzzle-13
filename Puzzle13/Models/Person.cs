024a401e6a36e009ddfd0d004cd356520add9487
Carl Franklin <carl@appvnext.com>
2023-11-11 18:28:03 -0500

    Added a ForeignKey property

--- Puzzle13/Models/Person.cs
+++ Puzzle13/Models/Person.cs
@@ -5,4 +5,5 @@
     public string LastName { get; set; } = string.Empty;
     public string Bio { get; set; } = string.Empty;
     public string PhotoUrl { get; set; } = string.Empty;
+    public string ForeignKey { get; set; } = string.Empty;
 }
